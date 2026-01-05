5de7c8c0962ea6461fc145a4d6b0278f92a11d73
Aren <aren.permadi@digipen.edu>
2026-01-04 23:10:35 -0800

    added acceleration and world boxes
    - minor edits to the path and timing of drops as well

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
 const GRAVITY = 250.0
-const WALK_SPEED = 250.0
+const WALK_SPEED = 550.0
+const ACCELERATION = 450.0
 
 func _physics_process(delta: float) -> void:
 	velocity.y += delta * GRAVITY
 	
 	if (Input.is_action_pressed("ui_left")):
-		velocity.x = -WALK_SPEED
+		velocity.x = move_toward(velocity.x, -WALK_SPEED, ACCELERATION * delta)
 	elif (Input.is_action_pressed("ui_right")):
-		velocity.x = WALK_SPEED
+		velocity.x = move_toward(velocity.x, WALK_SPEED, ACCELERATION * delta)
 	else:
 		velocity.x = 0
 		
